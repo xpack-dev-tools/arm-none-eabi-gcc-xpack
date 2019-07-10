bc5c288827ccd8c11a62c75c59f83779bab02b2f
Liviu Ionescu <ilg@livius.net>
2019-07-10 11:38:55 +0300

    git-clone: use -xpack.git

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/arm-none-eabi-gcc-build.git"
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/arm-none-eabi-gcc-build.git "${HOME}/Downloads/arm-none-eabi-gcc-build.git"
+rm -rf "${HOME}/Downloads/arm-none-eabi-gcc-xpack.git"
+git clone --recurse-submodules https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack.git "${HOME}/Downloads/arm-none-eabi-gcc-xpack.git"
