bd9ba60449c2b8ff7fa74e4e2320476d677597bb
Liviu Ionescu <ilg@livius.net>
2020-01-25 17:26:07 +0200

    scripts/common-versions: cosmetics

--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -1,3 +1,17 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPacks distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2019 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software 
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# Helper script used in the second edition of the GNU MCU Eclipse build 
+# scripts. As the name implies, it should contain only functions and 
+# should be included with 'source' by the container build scripts.
+
+# -----------------------------------------------------------------------------
 
 function prepare_versions()
 {
@@ -310,3 +324,5 @@ function prepare_versions()
     export PYTHON3_FOLDER_NAME="Python-${PYTHON3_VERSION}"
   fi
 }
+
+# -----------------------------------------------------------------------------
