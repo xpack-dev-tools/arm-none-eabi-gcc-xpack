1fb97560a114bcd260cdc87cad917f2092e5cff7
Liviu Ionescu <ilg@livius.net>
2020-02-17 21:38:44 +0200

    build.sh: xbb docker v3.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -65,7 +65,10 @@ source "${defines_script_path}"
 
 host_detect
 
-docker_images
+docker_linux64_image=${docker_linux64_image:-"ilegeul/ubuntu:amd64-12.04-xbb-v3.1"}
+docker_linux32_image=${docker_linux32_image:-"ilegeul/ubuntu:i386-12.04-xbb-v3.1"}
+docker_linux_arm64_image=${docker_linux_arm64_image:-"ilegeul/ubuntu:arm64-16.04-xbb-v3.1"}
+docker_linux_arm32_image=${docker_linux_arm32_image:-"ilegeul/ubuntu:armhf-16.04-xbb-v3.1"}
 
 # -----------------------------------------------------------------------------
 
