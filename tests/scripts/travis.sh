a314cafd96d681b3d9adfd93e11303612687e4dc
Liviu Ionescu <ilg@livius.net>
2020-04-01 12:12:28 +0300

    tests/scripts: add docker_run_test

--- tests/scripts/travis.sh
+++ tests/scripts/travis.sh
@@ -34,4 +34,59 @@ script_folder_name="$(basename "${script_folder_path}")"
 
 # =============================================================================
 
-docker run hello-world
+function docker_run_test() {
+  local image_name="$1"
+
+  local container_work_folder_path="/Host/Work"
+  local container_repo_folder_path="/Host/repo"
+
+  docker run \
+    --tty \
+    --volume "${HOME}/Work:${container_work_folder_path}" \
+    --volume "${TRAVIS_BUILD_DIR}:${container_repo_folder_path}" \
+    "${image_name}" \
+    /bin/bash ${DEBUG} "${container_repo_folder_path}/tests/scripts/container-test.sh" \
+      "${image_name}" \
+      "${base_url}"
+}
+
+# =============================================================================
+
+base_url="$1"
+echo "Base URL: ${base_url}"
+
+mkdir -p "${HOME}/Work"
+
+host_platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+host_machine=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+if [ "${host_platform}" == "Linux" ]
+then
+  if [ "${host_machine}" == "x86_64" ]
+  then
+    echo "Testing Intel Linux"
+
+    docker_run_test "ubuntu:20.04" 
+    
+    exit 0
+  elif [ "${host_machine}" == "aarch64" ]
+  then
+    echo "Testing Arm Linux"
+    docker run hello-world  
+    exit 0
+  else
+    echo "${host_machine} not supported"
+    exit 1
+  fi
+elif [ "${host_platform}" == "Darwin" ]
+then
+    echo "Testing macOS"
+    exit 0
+else
+  # TODO: add support for Windows
+  echo "${host_platform} not supported"
+  exit 1
+fi
+
+echo "Should not get here"
+exit 1
