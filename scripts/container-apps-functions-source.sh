372d1b0626db8bed5581e70f43e973ac9f5abc2f
Liviu Ionescu <ilg@livius.net>
2020-10-11 17:41:55 +0300

    container-apps: add rpath to LDFLAGS

--- scripts/container-apps-functions-source.sh
+++ scripts/container-apps-functions-source.sh
@@ -80,6 +80,9 @@ function build_binutils()
       if [ "${TARGET_PLATFORM}" == "win32" ]
       then
         LDFLAGS+=" -Wl,${XBB_FOLDER_PATH}/mingw/lib/CRT_glob.o"
+      elif [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
       fi
       if [ "${IS_DEVELOP}" == "y" ]
       then
@@ -252,6 +255,10 @@ function build_gcc_first()
       CFLAGS="${XBB_CFLAGS_NO_W}"
       CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
       LDFLAGS="${XBB_LDFLAGS_APP}" 
+      if [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+      fi      
       if [ "${IS_DEVELOP}" == "y" ]
       then
         LDFLAGS+=" -v"
@@ -688,6 +695,10 @@ function build_gcc_final()
       CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
 
       LDFLAGS="${XBB_LDFLAGS_APP}" 
+      if [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+      fi      
       # Do not add CRT_glob.o here, it will fail with already defined,
       # since it is already handled by --enable-mingw-wildcard.
       if [ "${IS_DEVELOP}" == "y" ]
@@ -1118,6 +1129,7 @@ function build_gdb()
       elif [ "${TARGET_PLATFORM}" == "linux" ]
       then
         LDFLAGS="${XBB_LDFLAGS_APP}"
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
         LIBS=""
       fi
 
