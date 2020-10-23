12cb3fec73de7f44c4b91a016823cab1afd50fd3
Liviu Ionescu <ilg@livius.net>
2020-10-23 10:34:07 +0300

    container-test: zypper -y

--- tests/scripts/container-test.sh
+++ tests/scripts/container-test.sh
@@ -87,8 +87,8 @@ then
   yum install -y -q git curl tar gzip redhat-lsb-core binutils
 elif [[ ${image_name} == *opensuse* ]]
 then
-  zypper -q refresh
-  zypper -q update
+  zypper -q refresh -y
+  zypper -q update -y
   zypper -q in -y git-core curl tar gzip lsb-release binutils
   zypper -q in -y gnu-getopt
 elif [[ ${image_name} == *manjaro* ]]
