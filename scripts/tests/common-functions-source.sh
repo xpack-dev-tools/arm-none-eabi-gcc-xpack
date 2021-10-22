95da3ea0367486832c6194d96b7b322cc0236ca3
Liviu Ionescu <ilg@livius.net>
2021-10-22 13:47:51 +0300

    common-functions: echo env

--- scripts/tests/common-functions-source.sh
+++ scripts/tests/common-functions-source.sh
@@ -22,6 +22,9 @@ function run_tests()
   GCC_VERSION="$(echo "${RELEASE_VERSION}" | sed -e 's|-.*||')"
   GCC_VERSION_MAJOR=$(echo ${GCC_VERSION} | sed -e 's|\([0-9][0-9]*\)\..*|\1|')
 
+  echo
+  env
+
   test_binutils
 
   test_gcc
@@ -36,7 +39,10 @@ function update_image()
   local image_name="$1"
   
   # Make sure that the minimum prerequisites are met.
-  if [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
+  if [[ ${image_name} == github-actions-ubuntu* ]]
+  then
+    : # sudo apt-get -qq install -y XXX
+  elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
   then
     run_verbose apt-get -qq update 
     run_verbose apt-get -qq install -y git-core curl tar gzip lsb-release binutils
