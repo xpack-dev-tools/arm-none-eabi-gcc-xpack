1646a03974d2e65eaa233e34680918250afa26f5
Liviu Ionescu <ilg@livius.net>
2021-11-18 10:54:33 +0200

    use .patch.diff

--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -173,7 +173,7 @@ function build_versions()
     if [[ "${RELEASE_VERSION}" =~ 10\.3\.1-2\.3 ]] \
        && [ "${TARGET_PLATFORM}" == "darwin" -a "${TARGET_ARCH}" == "arm64" ]
     then
-      GCC_PATCH="gcc-10.3.1-apple-silicon.patch"
+      GCC_PATCH="gcc-10.3.1-apple-silicon.patch.diff"
     fi
 
     # -------------------------------------------------------------------------
