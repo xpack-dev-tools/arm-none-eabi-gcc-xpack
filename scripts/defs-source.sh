25afd3c00e1d9b5d3a262757ad78ecfb62e81918
Liviu Ionescu <ilg@livius.net>
2021-11-23 12:17:38 +0200

    re-generate workflows

--- scripts/defs-source.sh
+++ scripts/defs-source.sh
@@ -3,7 +3,7 @@
 #   (https://xpack.github.io)
 # Copyright (c) 2019 Liviu Ionescu.
 #
-# Permission to use, copy, modify, and/or distribute this software 
+# Permission to use, copy, modify, and/or distribute this software
 # for any purpose is hereby granted, under the terms of the MIT license.
 # -----------------------------------------------------------------------------
 
