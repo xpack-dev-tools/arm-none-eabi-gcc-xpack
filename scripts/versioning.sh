9f60bfabdaa288e1b9b583498240f3004e00886f
Liviu Ionescu <ilg@livius.net>
2022-12-08 11:55:38 +0200

    versioning.sh: explicit XBB_PYTHON3_SRC_FOLDER_NAME

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -222,6 +222,9 @@ function build_application_versioned_components()
     XBB_PYTHON3_VERSION_MAJOR=$(echo ${XBB_PYTHON3_VERSION} | sed -e 's|\([0-9]\)\..*|\1|')
     XBB_PYTHON3_VERSION_MINOR=$(echo ${XBB_PYTHON3_VERSION} | sed -e 's|\([0-9]\)\.\([0-9][0-9]*\)\..*|\2|')
 
+    # Explicit, since it is also used in python3_copy_syslibs
+    XBB_PYTHON3_SRC_FOLDER_NAME="Python-${XBB_PYTHON3_VERSION}"
+
     # -------------------------------------------------------------------------
     # Build the native dependencies.
 
