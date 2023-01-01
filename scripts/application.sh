64ee787e8dd7cdca3224bc6ffd31ccec48b39082
Liviu Ionescu <ilg@livius.net>
2023-01-01 21:00:06 +0200

    application.sh: add zstd dep

--- scripts/application.sh
+++ scripts/application.sh
@@ -23,7 +23,7 @@ XBB_APPLICATION_DISTRO_TOP_FOLDER=${XBB_APPLICATION_DISTRO_TOP_FOLDER:-"xPacks"}
 XBB_APPLICATION_DESCRIPTION="${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME}"
 
 declare -a XBB_APPLICATION_DEPENDENCIES=( )
-declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( zlib gmp mpfr mpc isl libiconv xz  binutils-cross gcc-cross newlib-cross expat gettext gpm ncurses readline bzip2 libffi mpdecimal sqlite libxcrypt openssl python3 gdb-cross )
+declare -a XBB_APPLICATION_COMMON_DEPENDENCIES=( zlib gmp mpfr mpc isl libiconv xz zstd binutils-cross gcc-cross newlib-cross expat gettext gpm ncurses readline bzip2 libffi mpdecimal sqlite libxcrypt openssl python3 gdb-cross )
 
 # -----------------------------------------------------------------------------
 
