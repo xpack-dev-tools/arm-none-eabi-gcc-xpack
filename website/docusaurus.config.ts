438a40b718ecf55047356b63aaf003f1111ea72c
Liviu Ionescu <ilg@livius.net>
2025-01-10 13:20:21 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -89,7 +89,8 @@ console.log('customFields: ' + util.inspect(customFields));
 // ----------------------------------------------------------------------------
 
 const config: Config = {
-  title: 'xPack GNU Arm Embedded GCC',
+  title: 'xPack GNU Arm Embedded GCC' +
+    ((process.env.DOCUSAURUS_IS_PREVIEW === 'true') ? ' (preview)' : ''),
   tagline: 'A binary distribution of GNU Arm Embedded GCC',
   // Explicitly set in headTags.
   // favicon: '/img/favicon.ico',
@@ -260,7 +261,7 @@ const config: Config = {
       items: [
         {
           to: '/',
-          // label: 'Home',
+          label: 'arm-none-eabi-gcc',
           className: 'header-home-link',
           position: 'left'
         },
