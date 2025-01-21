94a177d4cbaef12d468241dbf5135a8f6d0da29c
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:21:21 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/arm-none-eabi-gcc@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/arm-none-eabi-gcc@14.2.1-1.1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
