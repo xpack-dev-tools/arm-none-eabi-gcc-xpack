f671d11dd064f20f285a3091c8bd3abc653416f7
Liviu Ionescu <ilg@livius.net>
2025-01-26 21:53:41 +0200

    website: rework with _project

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -7,8 +7,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // import logger from '@docusaurus/logger';
 import util from 'node:util';
 
-import {redirects} from './docusaurus-config-redirects'
-import {getCustomFields} from './customFields'
+import {redirects} from './docusaurus-config-redirects';
+import {getCustomFields} from './customFields';
 
 // The node.js modules cannot be used in modules imported in browser code:
 // webpack < 5 used to include polyfills for node.js core modules by default.
