b4b72d9d31b506e8451aaf4bc540edb0a6e12ba7
Liviu Ionescu <ilg@livius.net>
2025-02-07 15:43:27 +0200

    website: re-generate commons

---
+++ website/src/libs/dateYYYYMMDD.ts
@@ -0,0 +1,17 @@
+/*
+ * DO NOT EDIT!
+ * Automatically generated from docusaurus-template-liquid/templates/docusaurus.
+ *
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2024 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+export default function dateYYYYMMDD(): string {
+  return (new Date()).toISOString().slice(0,10).replace(/-/g,"");
+}
